c3b70fff1f4e83aef4e2231318e8396dd2b6465a
Ilya Dudarek <44503765+snamenatel@users.noreply.github.com>
2022-05-18 09:42:21 +0700

    Time: 82 ms (64.63%), Space: 43.4 MB (78.27%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const nextGreaterElement = function (nums1, nums2) {
+  ge = getGreaterElList(nums2);
+  return nums1.map(element => {
+      return ge[nums2.indexOf(element)];
+  });
+};
+
+function getGreaterElList(list)
+{
+    const result = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      let nextGreater = -1;
+      for (let j = i + 1; j < list.length; j++) {
+        if (list[j] > list[i]) {
+            nextGreater = list[j];
+          break;
+        }
+      }
+      result.unshift(nextGreater);
+    }
+    return result
+}

4e0fe3c428e1342f00dfadbae8c7a799106a6f59
Ilya Dudarek <44503765+snamenatel@users.noreply.github.com>
2022-05-19 08:46:00 +0700

    Time: 143 ms (12.10%), Space: 45.7 MB (35.89%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function (mat, r, c) {
+  const flat = mat.flat();
+  if(r * c !== flat.length) {
+    return mat;
+  }
+  const res = [];
+  let idx = 0;
+  for(let i = 0; i < r; i++) {
+    res[i] = [];
+    for(let j = 0; j < c; j++) {
+      if(flat[idx] === undefined) {
+        return mat;
+      }
+      res[i][j] = flat[idx];
+      idx++
+    }
+  }
+  return res;
+};

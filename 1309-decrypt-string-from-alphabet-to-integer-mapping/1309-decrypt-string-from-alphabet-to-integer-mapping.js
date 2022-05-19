8bd11623a6c49245b20a81888f92ee0debc1b7e3
Ilya Dudarek <44503765+snamenatel@users.noreply.github.com>
2022-05-19 08:22:39 +0700

    Time: 65 ms (79.46%), Space: 42.1 MB (74.33%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var freqAlphabets = function(s) {
+    const nums = s.match(/((?:\d{2}(?=#))|(?:\d{1}))/g).map(el => +el+96);
+    return String.fromCharCode(...nums);
+};
